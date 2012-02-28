b47f6490ba9f0f5ad2d04dcad3946349a02f9d2a
Beoran <beoran@rubyforge.org>
2012-02-28 20:55:56 +0100

    Adding all relevant files and supporting libraries.

---
+++ src/org/katawashojoufighter/Main.java
@@ -0,0 +1,25 @@
+package org.katawashojoufighter;
+
+import org.newdawn.slick.BasicGame; 
+import org.newdawn.slick.GameContainer; 
+import org.newdawn.slick.Graphics; 
+import org.newdawn.slick.SlickException; 
+import org.newdawn.slick.AppGameContainer;
+
+public class Main extends BasicGame { 
+	public Main() { super("SimpleTest"); } 
+	@Override public void init(GameContainer container) 
+	throws SlickException {} 
+	@Override public void update(GameContainer container, int delta) 
+    throws SlickException {} 
+	@Override public void render(GameContainer container, Graphics g) 
+    throws SlickException 
+    { g.drawString("Hello, Slick world!", 0, 100); } 
+	public static void main(String[] args) { 
+		try { 
+			AppGameContainer app = new AppGameContainer(new Main()); app.start(); 
+		} catch (SlickException e) { 
+			e.printStackTrace(); 
+		} 
+	} 
+}
